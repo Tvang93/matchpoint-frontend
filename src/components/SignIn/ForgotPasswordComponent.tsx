0f2597e6dec54c837277a1246ab8378a9debeb0e
Tvang93 <vangt39@gmail.com>
2025-04-01 09:14:32 -0700

    prettier

--- src/components/SignIn/ForgotPasswordComponent.tsx
+++ src/components/SignIn/ForgotPasswordComponent.tsx
@@ -1,15 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const ForgotPasswordComponent: React.FC<TSignInProp> = ({ switchComponent }) => {
+const ForgotPasswordComponent: React.FC<TSignInProp> = ({
+  switchComponent,
+}) => {
   return (
-    <div className='flex flex-col items-center text-[#E1FF00] gap-9 py-12 px-28 w-full'>
-      <h1 className='text-3xl'>Forgot Password</h1>
+    <div className="flex flex-col items-center text-[#E1FF00] gap-9 py-12 px-28 w-full">
+      <h1 className="text-3xl">Forgot Password</h1>
       <p>Please Enter in Your Email.</p>
-      <input className='border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm' id="passwordRecoveryInputField" type="text" placeholder='Email' />
-      <button className='bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("New Password")}>Send Email</button>
-      <button className='bg-transparent border-1 border-[#E1FF00] text-[#E1FF00] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("Sign In")}>Back to Sign In</button>
+      <input
+        className="border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm"
+        id="passwordRecoveryInputField"
+        type="text"
+        placeholder="Email"
+      />
+      <button
+        className="bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer"
+        onClick={() => switchComponent("New Password")}
+      >
+        Send Email
+      </button>
+      <button
+        className="bg-transparent border-1 border-[#E1FF00] text-[#E1FF00] rounded-[20px] w-full hover:cursor-pointer"
+        onClick={() => switchComponent("Sign In")}
+      >
+        Back to Sign In
+      </button>
     </div>
   );
 };
 
-export default ForgotPasswordComponent
+export default ForgotPasswordComponent;
--- src/components/SignIn/SignInComponent.tsx
+++ src/components/SignIn/SignInComponent.tsx
@@ -1,23 +1,44 @@
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const SignInComponent: React.FC<TSignInProp> = ({ switchComponent }) => {
-  const {push} = useRouter()
+  const { push } = useRouter();
 
   return (
-    <div className='flex flex-col items-center text-[#E1FF00] gap-9 py-12 px-28 w-full'>
-      <h1 className='text-3xl'>Sign In</h1>
-      <input className='border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm' id="usernameInputField" type="text" placeholder='Username' />
-      <input className='border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm' id="passwordInputField" type="text" placeholder='Password' />
-      <button className='bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer'>Login</button>
-      <p className='text-[#4BB4F1] hover:cursor-pointer' onClick={() => switchComponent("Forgot Password")}>Forgot Password?</p>
-      <div className='flex gap-2'>
+    <div className="flex flex-col items-center text-[#E1FF00] gap-9 py-12 px-28 w-full">
+      <h1 className="text-3xl">Sign In</h1>
+      <input
+        className="border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm"
+        id="usernameInputField"
+        type="text"
+        placeholder="Username"
+      />
+      <input
+        className="border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm"
+        id="passwordInputField"
+        type="text"
+        placeholder="Password"
+      />
+      <button className="bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer">
+        Login
+      </button>
+      <p
+        className="text-[#4BB4F1] hover:cursor-pointer"
+        onClick={() => switchComponent("Forgot Password")}
+      >
+        Forgot Password?
+      </p>
+      <div className="flex gap-2">
         <p>Don't Have An Account?</p>
-        <p className='text-[#4BB4F1] hover:cursor-pointer' onClick={() => switchComponent("Create Account")}>Sign Up Now</p>
+        <p
+          className="text-[#4BB4F1] hover:cursor-pointer"
+          onClick={() => switchComponent("Create Account")}
+        >
+          Sign Up Now
+        </p>
       </div>
-      
     </div>
   );
 };
 
-export default SignInComponent
+export default SignInComponent;
