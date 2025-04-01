575fe49bc55dc2972de3e080c0b396d2d82265c4
Tvang93 <vangt39@gmail.com>
2025-04-01 08:11:52 -0700

    fixed links

--- src/components/SignIn/AccountConfirmationComponent.tsx
+++ src/components/SignIn/AccountConfirmationComponent.tsx
@@ -4,7 +4,7 @@ const AccountConfirmationComponent: React.FC<TSignInProp> = ({switchComponent}) 
   return (
     <div className='flex flex-col items-center text-[#E1FF00] gap-9 py-12 px-28 w-full'>
       <h1 className='text-3xl'>Free Sign Up</h1>
-      <p>Account Created</p>
+      <p>Account Created!</p>
       <button className='bg-transparent border-1 border-[#E1FF00] text-[#E1FF00] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("Sign In")}>Back to Sign In</button>
     </div>
   )
--- src/components/SignIn/ForgotPasswordComponent.tsx
+++ src/components/SignIn/ForgotPasswordComponent.tsx
@@ -6,7 +6,7 @@ const ForgotPasswordComponent: React.FC<TSignInProp> = ({ switchComponent }) => 
       <h1 className='text-3xl'>Forgot Password</h1>
       <p>Please Enter in Your Email.</p>
       <input className='border-1 border-[#E1FF00] bg-transparent ps-2 w-full hover:cursor-pointer focus:cursor-text rounded-sm' id="passwordRecoveryInputField" type="text" placeholder='Email' />
-      <button className='bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("Sign In")}>Send Email</button>
+      <button className='bg-[#E1FF00] text-[#243451] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("New Password")}>Send Email</button>
       <button className='bg-transparent border-1 border-[#E1FF00] text-[#E1FF00] rounded-[20px] w-full hover:cursor-pointer' onClick={() => switchComponent("Sign In")}>Back to Sign In</button>
     </div>
   );
