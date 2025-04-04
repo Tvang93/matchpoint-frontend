e16d2edbe448da990fedca6f113b3f18d275bbb5
Tvang93 <vangt39@gmail.com>
2025-04-04 13:24:31 -0700

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col">
       <NavbarComponent />
-      <div className="relative w-full bg-[url(/assets/mp-hero-1.jpeg)] bg-cover bg-no-repeat bg-center min-h-180 flex justify-center items-end">
+      <div className="w-full bg-[url(/assets/mp-hero-1.jpeg)] bg-cover bg-no-repeat bg-center min-h-180 flex justify-center items-end">
         <div className="relative flex justify-center items-end bottom-40">
           <input className="bg-white py-2 px-5 text-3xl border-1 rounded-4xl w-180 " type="text" placeholder="Search Location"/>
         </div>
