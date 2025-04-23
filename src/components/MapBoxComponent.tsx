e39f1bf7e0d2e42919b4e47aecbdacca84c07d6d
Tvang93 <vangt39@gmail.com>
2025-04-23 15:21:14 -0700

    mapbox map with coords

--- src/components/MapBoxComponent.tsx
+++ src/components/MapBoxComponent.tsx
@@ -1,30 +1,59 @@
 'use client'
 
 import { mapbox } from '@/utils/DataServices'
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import mapboxgl from 'mapbox-gl'
 
+const INITIAL_CENTER = [
+  -74.0242,
+  40.6941
+]
+
 const MapBoxComponent = () => {
-    const mapRef = useRef()
-    const mapContainerRef = useRef()
+    const mapRef = useRef<mapboxgl.Map | null>(null)
+    const mapContainerRef = useRef<HTMLDivElement | null>(null)
+
+    const [latitude, setLatitude] = useState(INITIAL_CENTER[1])
+    const [longitude, setLongitude] = useState(INITIAL_CENTER[0])
+
 
     useEffect(() => {
       mapboxgl.accessToken = mapbox
+      if(!mapContainerRef.current) return
       mapRef.current = new mapboxgl.Map({
         container: mapContainerRef.current,
         center: [-74.0242, 40.6941],
-        zoom: 10.12
+        zoom: 12
       });
+
+      mapRef.current.on('move', () => {
+        if(mapRef.current){
+        // get the current center coordinates and zoom level from the map
+          const mapCenter = mapRef.current.getCenter()
   
+          // update state
+          setLatitude(mapCenter.lat)
+          setLongitude(mapCenter.lng)
+      }})
+
       return () => {
-        mapRef.current.remove()
+        if(mapRef.current){
+          mapRef.current.remove()
+        }
       }
     }, [])
 
   return (
     <div>
         <p>Testing page</p>
-        <div id='map-container' className='h-150 w-100' ref={mapContainerRef}></div>
+        <div className="sidebar">
+        Longitude: {longitude.toFixed(4)} | Latitude: {latitude.toFixed(4)}
+      </div>
+        <div id='map-container' className='relative h-150 w-100' ref={mapContainerRef}>
+          <div className='absolute top-[280px] left-[180px]'>
+            <img className='h-10 w-10' src="/assets/tennisIcon.png" alt="" />
+          </div>
+        </div>
     </div>
   )
 }
